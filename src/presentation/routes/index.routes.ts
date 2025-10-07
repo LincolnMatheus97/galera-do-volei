7d1a20779b5730924b40d77fcd79f7a31b1c3a2f
LincolnMatheus97 <linconlmatheus25@gmail.com>
2025-10-07 00:07:45 -0300

    :recycle: refactor: Refatora o router de index

--- src/presentation/routes/index.routes.ts
+++ src/presentation/routes/index.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import jogadorRoutes from './jogador.routes.js';
 import partidaRoutes from './partida.routes.js';
+import conviteRoutes from './convite.routes.js';
 
 const routes = Router();
 
 routes.use(jogadorRoutes);
 routes.use(partidaRoutes);
+routes.use(conviteRoutes);
 
 export default routes;
