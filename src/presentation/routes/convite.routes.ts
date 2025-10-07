81162707dfdd52b1cfd2e8164e438f10fb456ed4
LincolnMatheus97 <linconlmatheus25@gmail.com>
2025-10-07 00:06:30 -0300

    :recycle: refactor: Refatora o router de convite

---
+++ src/presentation/routes/convite.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import * as conviteController from '../controllers/convite.controller.js';
+
+const router = Router();
+
+router.get('/convites', conviteController.listar);
+router.post('/convites', conviteController.criar);
+router.delete('/convites/:id', conviteController.excluir);
+router.post('/convites/:id/aceitar', conviteController.aceitar);
+router.post('/convites/:id/rejeitar', conviteController.recusar);
+
+export default router;
