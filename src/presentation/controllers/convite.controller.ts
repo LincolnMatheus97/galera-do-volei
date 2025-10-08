4668130875740575ea467a4eceb6b44b376ac9b3
LincolnMatheus97 <linconlmatheus25@gmail.com>
2025-10-08 00:31:26 -0300

    :recycle: refactor: Refatora o controller de convite

--- src/presentation/controllers/convite.controller.ts
+++ src/presentation/controllers/convite.controller.ts
@@ -1,79 +1,83 @@
 import type { Request, Response } from "express";
 import z from "zod";
-import * as conviteServices from '../../application/service/convite.service.js';
+import { conviteService } from "../../application/service/convite.service.js";
 
 const criarConviteSchema = z.object({
     nome_remetente: z.string(),
     nome_destinatario: z.string()
 });
 
-export const listar = (req: Request, res: Response) => {
-    const todosOsConvites = conviteServices.listarConvites();
-    return res.status(200).json(todosOsConvites);
-}
+class ConviteController {
+    async listar(req: Request, res: Response) {
+        const todosOsConvites = await conviteService.listarConvites();
+        return res.status(200).json(todosOsConvites);
+    }
 
-export const criar = (req: Request, res: Response) => {
-    try {
-        const data = criarConviteSchema.parse(req.body);
-        const novoConvite = conviteServices.criarConvite({nome: data.nome_remetente}, {nome: data.nome_destinatario});
-        return res.status(201).json(novoConvite);
-    } catch (error) {
-        return res.status(400).json({message: "Dados inválidos", erros: error});
+    async criar(req: Request, res: Response) {
+        try {
+            const data = criarConviteSchema.parse(req.body);
+            const novoConvite = await conviteService.criarConvite({nome: data.nome_remetente}, {nome: data.nome_destinatario});
+            return res.status(201).json(novoConvite);
+        } catch (error) {
+            return res.status(400).json({message: "Dados inválidos", erros: error});
+        }
+    }
+
+    async excluir (req: Request, res: Response) {
+        const idParam = req.params.id ?? '';
+        const idParaDeletar = parseInt(idParam, 10);
+
+        if (isNaN(idParaDeletar)) {
+            return res.status(400).json({message: "ID inválido. Por favor digite um ID válido"});
+        }
+
+        if (!await conviteService.conviteExiste(idParaDeletar)) {
+            return res.status(404).json({message: "Convite não existe!"});
+        }
+
+        await conviteService.excluirConvite(idParaDeletar);
+        return res.status(204).json({message: `Convite de id ${idParaDeletar} excluído com sucesso`});
+    }
+
+    async aceitar (req: Request, res: Response) {
+        const idParam = req.params.id ?? '';
+        const idParaAceitar = parseInt(idParam, 10);
+
+        if (isNaN(idParaAceitar)) {
+            return res.status(400).json({message: "ID inválido. Por favor digite um ID válido"});
+        }
+
+        if (!await conviteService.conviteExiste(idParaAceitar)) {
+            return res.status(404).json({message: "Convite não existe!"});
+        }
+
+        try {
+            await conviteService.aceitarConvite(idParaAceitar)
+            res.status(200).json({ message: `Jogador aceito na partida!` });
+        } catch (error) {
+            return res.status(400).json({message: "Dados inválidos", erros: error});
+        }
+    }
+
+    async recusar (req: Request, res: Response) {
+        const idParam = req.params.id ?? '';
+        const idParaAceitar = parseInt(idParam, 10);
+
+        if (isNaN(idParaAceitar)) {
+            return res.status(400).json({message: "ID inválido. Por favor digite um ID válido"});
+        }
+
+        if (!await conviteService.conviteExiste(idParaAceitar)) {
+            return res.status(404).json({message: "Convite não existe!"});
+        }
+
+        try {
+            await conviteService.rejeitarConvite(idParaAceitar)
+            res.status(200).json({ message: `Jogador rejeitado na partida!` });
+        } catch (error) {
+            return res.status(400).json({message: "Dados inválidos", erros: error});
+        }
     }
 }
 
-export const excluir = (req: Request, res: Response) => {
-    const idParam = req.params.id ?? '';
-    const idParaDeletar = parseInt(idParam, 10);
-
-    if (isNaN(idParaDeletar)) {
-        return res.status(400).json({message: "ID inválido. Por favor digite um ID válido"});
-    }
-
-    if (!conviteServices.conviteExiste(idParaDeletar)) {
-        return res.status(404).json({message: "Convite não existe!"});
-    }
-
-    conviteServices.excluirConvite(idParaDeletar);
-    return res.status(204).json({message: `Convite de id ${idParaDeletar} excluído com sucesso`});
-}
-
-export const aceitar = (req: Request, res: Response) => {
-    const idParam = req.params.id ?? '';
-    const idParaAceitar = parseInt(idParam, 10);
-
-    if (isNaN(idParaAceitar)) {
-        return res.status(400).json({message: "ID inválido. Por favor digite um ID válido"});
-    }
-
-    if (!conviteServices.conviteExiste(idParaAceitar)) {
-        return res.status(404).json({message: "Convite não existe!"});
-    }
-
-    try {
-        conviteServices.aceitarConvite(idParaAceitar)
-        res.status(200).json({ message: `Jogador aceito na partida!` });
-    } catch (error) {
-        return res.status(400).json({message: "Dados inválidos", erros: error});
-    }
-}
-
-export const recusar = (req: Request, res: Response) => {
-    const idParam = req.params.id ?? '';
-    const idParaAceitar = parseInt(idParam, 10);
-
-    if (isNaN(idParaAceitar)) {
-        return res.status(400).json({message: "ID inválido. Por favor digite um ID válido"});
-    }
-
-    if (!conviteServices.conviteExiste(idParaAceitar)) {
-        return res.status(404).json({message: "Convite não existe!"});
-    }
-
-    try {
-        conviteServices.rejeitarConvite(idParaAceitar)
-        res.status(200).json({ message: `Jogador rejeitado na partida!` });
-    } catch (error) {
-        return res.status(400).json({message: "Dados inválidos", erros: error});
-    }
-}
+export const conviteController = new ConviteController();
