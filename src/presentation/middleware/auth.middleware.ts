27581404073a2b0f0529bd90b4dca662ad9fcc81
LincolnMatheus97 <linconlmatheus25@gmail.com>
2025-10-09 01:22:08 -0300

    :recycle: refactor: Refatora o middleware de autenticação

---
+++ src/presentation/middleware/auth.middleware.ts
@@ -0,0 +1,18 @@
+import type { NextFunction, Request, Response } from "express";
+import { HttpException } from "./HttpException.middleware.js";
+
+const token = 'volei123';
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+        throw new HttpException('Token de acesso não informado.', 401);
+    }
+
+    if (authorization !== token) {
+        throw new HttpException('Token de acesso inválido.', 401);
+    }
+
+    next();
+}
