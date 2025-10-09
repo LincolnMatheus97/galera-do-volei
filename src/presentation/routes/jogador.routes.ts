72bd139ad21e8ea3c104705117663e798b31d1dc
LincolnMatheus97 <linconlmatheus25@gmail.com>
2025-10-09 10:56:23 -0300

    :recycle: refactor: Refatora o router de jogador

--- src/presentation/routes/jogador.routes.ts
+++ src/presentation/routes/jogador.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
-import { jogadorController } from "../controllers/jogador.controller.js";
+import { criarJogadorSchema, edtDadosBasicosJogadorSchema, edtModeracaoJogadorSchema, jogadorController } from "../controllers/jogador.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
+import { validationMiddleware } from "../middleware/validation.middleware.js";
 
 const router = Router();
 
 router.get('/jogadores', jogadorController.listar);
-router.post('/jogadores', authMiddleware, jogadorController.criar);
+router.post('/jogadores', authMiddleware, validationMiddleware(criarJogadorSchema), jogadorController.criar);
 router.delete('/jogadores/:id', authMiddleware, jogadorController.deletar);
-router.patch('/jogadores/:id', authMiddleware, jogadorController.atualizarDados);
-router.patch('/jogadores/:id/moderador', authMiddleware, jogadorController.atualizarModerador);
+router.patch('/jogadores/:id', authMiddleware, validationMiddleware(edtDadosBasicosJogadorSchema), jogadorController.atualizarDados);
+router.patch('/jogadores/:id/moderador', authMiddleware, validationMiddleware(edtModeracaoJogadorSchema), jogadorController.atualizarModerador);
 
 export default router;
