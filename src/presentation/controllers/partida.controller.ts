70d68496315764db56bdea791550266a0e208f45
LincolnMatheus97 <linconlmatheus25@gmail.com>
2025-10-09 01:21:01 -0300

    :recycle: refactor: Refatora o controller de partida

--- src/presentation/controllers/partida.controller.ts
+++ src/presentation/controllers/partida.controller.ts
@@ -1,6 +1,7 @@
 import { type Request, type Response } from 'express';
 import z from 'zod';
 import { partidaService } from '../../application/service/partida.service.js';
+import { HttpException } from '../middleware/HttpException.middleware.js';
 
 const criarPartidaSchema = z.object({
     tipo: z.string(),
@@ -22,31 +23,23 @@ const addAvaliacaoSchema = z.object({
 });
 
 class PartidaController {
-    async listarPartidas(req: Request, res: Response){
+    async listarPartidas(req: Request, res: Response) {
         const todasAsPartidas = await partidaService.listarPartidas();
         res.status(200).json(todasAsPartidas);
     }
 
     async criarPartida(req: Request, res: Response) {
-        try {
-            const data = criarPartidaSchema.parse(req.body);
-            const novaPartida = await partidaService.criarPartida({nome :data.nome_moderador}, {tipo: data.tipo});
-            res.status(201).json(novaPartida);
-        } catch (error) {
-            res.status(400).json({message: "Dados inválidos", erros: error});
-        }
+        const data = criarPartidaSchema.parse(req.body);
+        const novaPartida = await partidaService.criarPartida({ nome: data.nome_moderador }, { tipo: data.tipo });
+        res.status(201).json(novaPartida);
     }
 
-    async deletar(req: Request, res: Response){
+    async deletar(req: Request, res: Response) {
         const idParam = req.params.id ?? '';
         const idParaDeletar = parseInt(idParam, 10);
 
         if (isNaN(idParaDeletar)) {
-            return res.status(400).json({message: "ID inválido. Por favor digite um ID válido"});
-        }
-
-        if (!await partidaService.partidaExiste(idParaDeletar)) {
-            return res.status(404).json({message: "Partida não existe!"});
+            throw new HttpException("ID inválido. Por favor digite um ID válido", 400);
         }
 
         await partidaService.excluirPartida(idParaDeletar);
@@ -58,20 +51,12 @@ class PartidaController {
         const idParaAtualizar = parseInt(idParam, 10);
 
         if (isNaN(idParaAtualizar)) {
-            return res.status(400).json({message: "ID inválido. Por favor digite um ID válido"});
+            throw new HttpException("ID inválido. Por favor digite um ID válido", 400);
         }
 
-        if (!await partidaService.partidaExiste(idParaAtualizar)) {
-            return res.status(404).json({message: "Partida não existe!"});
-        }
-
-        try {
-            const data = edtDadosBasicosPartidaSchema.parse(req.body);
-            const partidaAtualizada = await partidaService.atualizarDados(idParaAtualizar, {situacao: data.situacao});
-            res.status(200).json(partidaAtualizada);
-        } catch (error) {
-            res.status(400).json({message: "Dados inválidos", erros: error});
-        }
+        const data = edtDadosBasicosPartidaSchema.parse(req.body);
+        const partidaAtualizada = await partidaService.atualizarDados(idParaAtualizar, { situacao: data.situacao });
+        return res.status(200).json(partidaAtualizada);
     }
 
     async listarInscricoes(req: Request, res: Response) {
@@ -79,11 +64,7 @@ class PartidaController {
         const idParaListar = parseInt(idParam, 10);
 
         if (isNaN(idParaListar)) {
-            return res.status(400).json({message: "ID inválido. Por favor digite um ID válido"});
-        }
-
-        if (!await partidaService.partidaExiste(idParaListar)) {
-            return res.status(404).json({message: "Partida não existe!"});
+            throw new HttpException("ID inválido. Por favor digite um ID válido", 400);
         }
 
         const todasAsInscricoes = await partidaService.listarInscricoes(idParaListar);
@@ -95,20 +76,12 @@ class PartidaController {
         const idParaAdd = parseInt(idParam, 10);
 
         if (isNaN(idParaAdd)) {
-            return res.status(400).json({message: "ID inválido. Por favor digite um ID válido"});
+            throw new HttpException("ID inválido. Por favor digite um ID válido", 400);
         }
 
-        if (!await partidaService.partidaExiste(idParaAdd)) {
-            return res.status(404).json({message: "Partida não existe!"});
-        }
-
-        try {
-            const data = addInscricaoPartidaSchema.parse(req.body);
-            const novaInscricao = await partidaService.adicionarInscricao(idParaAdd, {nome: data.nome_jogador});
-            res.status(200).json(novaInscricao);
-        } catch (error) {
-            res.status(400).json({message: "Dados inválidos", erros: error});
-        }
+        const data = addInscricaoPartidaSchema.parse(req.body);
+        const novaInscricao = await partidaService.adicionarInscricao(idParaAdd, { nome: data.nome_jogador });
+        return res.status(200).json(novaInscricao);
     }
 
     async aceitarInscricao(req: Request, res: Response) {
@@ -116,19 +89,11 @@ class PartidaController {
         const idParaAceitar = parseInt(idParam, 10);
 
         if (isNaN(idParaAceitar)) {
-            return res.status(400).json({message: "ID inválido. Por favor digite um ID válido"});
+            throw new HttpException("ID inválido. Por favor digite um ID válido", 400);
         }
 
-        if (!await partidaService.partidaExiste(idParaAceitar)) {
-            return res.status(404).json({message: "Partida não existe!"});
-        }
-
-        try {
-            await partidaService.aceitarInscricao(idParaAceitar);
-            return res.status(200).json({message: 'Inscrição Aceita'});
-        } catch (error) {
-            res.status(400).json({message: "Dados inválidos", erros: error});
-        }
+        await partidaService.aceitarInscricao(idParaAceitar);
+        return res.status(200).json({ message: 'Inscrição Aceita' });
     }
 
     async recusarInscricao(req: Request, res: Response) {
@@ -136,40 +101,24 @@ class PartidaController {
         const idParaAceitar = parseInt(idParam, 10);
 
         if (isNaN(idParaAceitar)) {
-            return res.status(400).json({message: "ID inválido. Por favor digite um ID válido"});
+            throw new HttpException("ID inválido. Por favor digite um ID válido", 400);
         }
 
-        if (!await partidaService.partidaExiste(idParaAceitar)) {
-            return res.status(404).json({message: "Partida não existe!"});
-        }
-
-        try {
-            await partidaService.recusarInscricao(idParaAceitar)
-            res.status(200).json({ message: `Jogador foi recusado na partida!` });
-        } catch (error) {
-            res.status(400).json({message: "Dados inválidos", erros: error});
-        }
+        await partidaService.recusarInscricao(idParaAceitar)
+        res.status(200).json({ message: `Jogador foi recusado na partida!` });
     }
 
-    async criarAvaliacao (req: Request, res: Response) {
+    async criarAvaliacao(req: Request, res: Response) {
         const idParam = req.params.id ?? '';
         const idParaAdd = parseInt(idParam, 10);
 
         if (isNaN(idParaAdd)) {
-            return res.status(400).json({message: "ID inválido. Por favor digite um ID válido"});
+            throw new HttpException("ID inválido. Por favor digite um ID válido", 400);
         }
 
-        if (!await partidaService.partidaExiste(idParaAdd)) {
-            return res.status(404).json({message: "Partida não existe!"});
-        }
-
-        try {
-            const data = addAvaliacaoSchema.parse(req.body);
-            const novaAvaliacao = await partidaService.adicionarAvaliacao(idParaAdd, data.nota, data.comentario, {nome: data.nome_jogador});
-            res.status(201).json(novaAvaliacao);
-        } catch (error) {
-            res.status(400).json({message: "Dados inválidos", erros: error});
-        }
+        const data = addAvaliacaoSchema.parse(req.body);
+        const novaAvaliacao = await partidaService.adicionarAvaliacao(idParaAdd, data.nota, data.comentario, { nome: data.nome_jogador });
+        res.status(201).json(novaAvaliacao);
     }
 }
 
