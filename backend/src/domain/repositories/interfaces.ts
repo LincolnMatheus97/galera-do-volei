2e522aed8570e82e844767354b6c65cfa4d92c58
LincolnMatheus97 <linconlmatheus25@gmail.com>
2025-12-07 14:49:20 -0300

    :truck: chore: Move diretório de arquivos

--- backend/src/domain/repositories/interfaces.ts
+++ backend/src/domain/repositories/interfaces.ts
@@ -13,14 +13,14 @@ export interface IPartidaRepository {
     buscarPorId(id: number): Promise<any>;
     excluir(id: number): Promise<void>;
     atualizar(id: number, data: any): Promise<any>;
-    
-    // Métodos específicos
     buscarInscricoesPorPartida(partidaId: number): Promise<any[]>;
     verificarInscricaoExistente(partidaId: number, jogadorId: number): Promise<boolean>;
+    buscarInscricaoPorQrCode(qrCode: string): Promise<any>;
     adicionarInscricao(data: any): Promise<any>;
-    atualizarInscricao(id: number, status: string): Promise<void>;
+    atualizarInscricao(id: number, data: any): Promise<void>;
     buscarInscricaoPorId(id: number): Promise<any>;
     adicionarAvaliacao(data: any): Promise<any>;
+    realizarCheckIn(inscricaoId: number): Promise<void>;
 }
 
 export interface IConviteRepository {
@@ -30,4 +30,14 @@ export interface IConviteRepository {
     atualizarStatus(id: number, status: string): Promise<void>;
     excluir(id: number): Promise<void>;
     verificarDuplicidade(remetenteId: number, destinatarioId: number, partidaId: number): Promise<boolean>;
+}
+
+export interface ISocialRepository {
+    solicitarAmizade(solicitanteId: number, destinatarioId: number): Promise<any>;
+    aceitarAmizade(id: number): Promise<void>;
+    listarAmigos(usuarioId: number): Promise<any[]>;
+    buscarAmizade(id: number): Promise<any>;
+    enviarMensagem(remetenteId: number, destinatarioId: number, conteudo: string): Promise<any>;
+    listarMensagens(usuarioId: number): Promise<any[]>;
+    verificarAmizade(userA: number, userB: number): Promise<boolean>;
 }
