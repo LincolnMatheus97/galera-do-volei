ff69e716165fd4a53a73a5209e696ad1d0bea5ae
LincolnMatheus97 <linconlmatheus25@gmail.com>
2025-12-06 21:22:11 -0300

    :sparkles: feat: Adiciona factories na main layer

---
+++ backend/src/main/factories.ts
@@ -0,0 +1,13 @@
+import { JogadorService } from "../application/service/jogador.service.js";
+import { PartidaService } from "../application/service/partida.service.js";
+import { ConviteService } from "../application/service/convite.service.js";
+import { jogadorRepository } from "../persistence/repositories/PrismaJogadorRepository.js";
+import { partidaRepository } from "../persistence/repositories/PrismaPartidaRepository.js";
+import { conviteRepository } from "../persistence/repositories/PrismaConviteRepository.js";
+
+
+export const makeJogadorService = new JogadorService(jogadorRepository);
+
+export const makePartidaService = new PartidaService(partidaRepository, jogadorRepository);
+
+export const makeConviteService = new ConviteService(conviteRepository, jogadorRepository, partidaRepository);
