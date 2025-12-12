557b81e96242ec8db0ae2587a29d5173f9451432
LincolnMatheus97 <linconlmatheus25@gmail.com>
2025-12-12 17:41:53 -0300

    :recycle: refactor: Refatora o drive de app

--- backend/src/main/app.ts
+++ backend/src/main/app.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import routes from '../presentation/routes/index.routes.js';
 import { logMiddleware } from '../presentation/middleware/log.middleware.js';
 import { globalErrorMiddleware } from '../presentation/middleware/globalError.middleware.js';
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'user-id', 'is-moderador']
+}));
 
 app.use(express.json());
 app.use(logMiddleware);
